5cdcdb7b107b376f9033749ca2c8202bcfaea7f8
parsasafaie <parsasafaie.2568@proton.me>
2025-01-29 14:25:21 +0330

    add database handling for signup school

--- python/main.py
+++ python/main.py
@@ -1,6 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'mysql://root:sapprogram2583@185.4.28.110:5000/sap'
+)
+db = SQLAlchemy(app)
+
+class School(db.Model):
+    __tablename__ = 'schools'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    school_name = db.Column(db.String(100), nullable=False)
+    school_code = db.Column(db.String(100), nullable=False)
+    manager_personal_code = db.Column(db.String(100), nullable=False)
+    province = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, school_name, school_code, manager_personal_code, province, city):
+        self.school_name = school_name
+        self.school_code = school_code
+        self.manager_personal_code = manager_personal_code
+        self.province = province
+        self.city = city
+
+
+@app.before_request
+def create_tables():
+    db.create_all()
 
 @app.route('/')
 def go_to_home():
@@ -10,8 +37,25 @@ def go_to_home():
 def go_to_login():
     return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def go_to_signup():
+    if request.method == 'POST':
+        school_name = request.form['school_name']
+        school_code = request.form['school_code']
+        manager_personal_code = request.form['manager_personal_code']
+        province = request.form['province']
+        city = request.form['city']
+
+        new_school = School(school_name=school_name, 
+                            school_code=school_code,
+                            manager_personal_code=manager_personal_code,
+                            province=province,
+                            city=city)
+        db.session.add(new_school)
+        db.session.commit()
+        
+        return redirect(url_for('go_to_notify_user'))
+    
     return render_template('signup.html')
 
 @app.route('/notify_user')
@@ -37,6 +81,8 @@ def go_to_panel_teachers():
 @app.route('/panel_students')
 def go_to_panel_students():
     return render_template('management_panel/students.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
