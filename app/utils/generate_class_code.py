2e426784961d8931ca2d0605c5b90b217c021749
parsasafaie <parsasafaie.2568@proton.me>
2025-02-24 09:10:31 +0330

    update class code generation

--- app/utils/generate_class_code.py
+++ app/utils/generate_class_code.py
@@ -1,11 +1,3 @@
-def generate_class_code(school_code:str, class_name:str):
-    part1 = str(hex(int(school_code)))[2:]
-    
-    part2 = ''
-    for char in class_name:
-        num = ord(char)
-        part2 = part2 + str(num)
-    part2 = str(hex(int(part2)))[2:]
-    
-    code = part1 + '#' + part2
-    return code
+generate_class_code = lambda school_code, class_name, key="crax6ix" : f"{hex(int(school_code))[2:]}#{'-'.join(hex(ord(char) ^ ord(key[i % len(key)]))[2:] for i, char in enumerate(class_name))}"
+
+reverse_class_code = lambda code, key="crax6ix": (str(int(code.split('#')[0], 16)), ''.join(chr(int(h, 16) ^ ord(key[i % len(key)])) for i, h in enumerate(code.split('#')[1].split('-'))))
