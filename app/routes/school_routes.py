0521c9c579e79b7681d5a5cc2d0bbcca95510bb4
parsasafaie <parsasafaie.2568@proton.me>
2025-02-28 14:46:13 +0330

    complete school info

--- app/routes/school_routes.py
+++ app/routes/school_routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, Blueprint
 from flask_login import login_required, current_user
 from app import db
+from app.models._class import Class
+from app.models.student import Student
 from app.models.school import School
 
 
@@ -16,5 +18,10 @@ def go_to_panel_home():
 @bp.route('/panel/school_info')
 @login_required
 def go_to_panel_school_info():
-    data = School.query.filter(School.school_code == current_user.school_code).first()
-    return render_template('school/school_info.html', data=data)
+    school = School.query.filter(School.school_code == current_user.school_code).first()
+    
+    teachers_count = len(eval(school.teachers))
+    classes_count = Class.query.filter(Class.school_code == school.school_code).count()
+    students_count = Student.query.filter(Student.school_code == school.school_code).count()
+
+    return render_template('school/school_info.html', data=school, tc = teachers_count, cc=classes_count, sc=students_count)
