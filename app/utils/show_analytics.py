2cb8d291939f432bd1d8a5a8b0c7232a1143c16b
parsasafaie <parsasafaie.2568@proton.me>
2025-04-07 19:08:06 +0330

    add compare students in analytics

--- app/utils/show_analytics.py
+++ app/utils/show_analytics.py
@@ -54,6 +54,24 @@ def compare_classes(classes: dict):
 
     fig.write_html("compare_classes.html")
 
+def compare_students(students: dict):
+    fig = px.bar(
+        x=list(students.keys()),
+        y=list(students.values()),
+        text=list(students.values()),
+        title="Compare Students"
+    )
+
+    fig = bar_style(fig, title="Compare Students", x_title="Student Name", y_title="Score")
+
+    fig.write_html("compare_students.html")
+
 
 if __name__=="__main__":
     compare_classes({'1051': 25.5, '1052': 84.3, '1053': 56.7, '1054': 96.3, '1051': 25.5, '1052': 84.3, '1053': 56.7, '1054': 96.3})
+    compare_students({
+    'Alex': 82.5, 'John': 75.2, 'Emma': 91.8, 'Max': 68.4, 'Liam': 85.0, 
+    'Sophia': 88.6, 'Ryan': 79.3, 'Mia': 92.1, 'Noah': 67.5, 'James': 73.8, 
+    'Ava': 84.2, 'Oliver': 77.9, 'Ella': 90.4, 'Jack': 69.1, 'Amelia': 81.3, 
+    'Lucas': 89.7, 'Charlotte': 93.5, 'Ethan': 74.6, 'Zoe': 70.8, 'Ben': 78.3
+    })
