ebd3750512607978e5d72dfc20e249cbf41ac654
parsasafaie <parsasafaie.2568@proton.me>
2025-04-07 19:31:23 +0330

    add student accuracy over week in analytics

--- app/utils/show_analytics.py
+++ app/utils/show_analytics.py
@@ -40,6 +40,50 @@ def bar_style(fig ,title, x_title, y_title):
     )
 
     return fig
+ 
+
+ #==================================================
+
+
+def line_style(fig ,title, x_title, y_title):
+    fig.update_traces(line=dict(color='#4ECDC4'))
+
+    fig.update_layout(
+
+        title=dict(
+            text=title,
+            font=dict(size=22, color='white', family='sans-serif'),
+            xanchor='center',
+            x=0.5
+        ),
+
+        font=dict(size=14, color='white', family='sans-serif'),
+
+        plot_bgcolor='#1E1E1E',
+        paper_bgcolor='#1E1E1E',
+
+        xaxis=dict(
+            title=dict(text=x_title, font=dict(color='white')),
+            showgrid=False,
+            tickfont=dict(color='white'),
+            linecolor='grey',
+            ticks='outside',
+        ),
+
+        yaxis=dict(
+            title=dict(text=y_title, font=dict(color='white')),
+            gridcolor='#444',
+            zeroline=False,
+            tickfont=dict(color='white'),
+            linecolor='grey'
+        ),
+
+    )
+
+    return fig
+
+
+ #==================================================
 
 
 def compare_classes(classes: dict):
@@ -54,6 +98,10 @@ def compare_classes(classes: dict):
 
     fig.write_html("compare_classes.html")
 
+
+ #==================================================
+
+
 def compare_students(students: dict):
     fig = px.bar(
         x=list(students.keys()),
@@ -67,11 +115,35 @@ def compare_students(students: dict):
     fig.write_html("compare_students.html")
 
 
+ #==================================================
+
+
+def student_accuracy_week(student_name:str, accuracy: dict):
+    fig = px.line(
+        x=list(accuracy.keys()),
+        y=list(accuracy.values()),
+        markers=True,
+        title=f"'{student_name}' Accuracy Over the Week",
+        labels={'x': 'Day', 'y': 'Accuracy (%)'}
+    )
+
+    fig = line_style(fig, title=f"'{student_name}' Accuracy Over the Week", x_title="Day", y_title="Accuracy (%)")
+
+    fig.write_html("student_accuracy_week.html")
+
+
 if __name__=="__main__":
     compare_classes({'1051': 25.5, '1052': 84.3, '1053': 56.7, '1054': 96.3, '1051': 25.5, '1052': 84.3, '1053': 56.7, '1054': 96.3})
+
     compare_students({
     'Alex': 82.5, 'John': 75.2, 'Emma': 91.8, 'Max': 68.4, 'Liam': 85.0, 
     'Sophia': 88.6, 'Ryan': 79.3, 'Mia': 92.1, 'Noah': 67.5, 'James': 73.8, 
     'Ava': 84.2, 'Oliver': 77.9, 'Ella': 90.4, 'Jack': 69.1, 'Amelia': 81.3, 
     'Lucas': 89.7, 'Charlotte': 93.5, 'Ethan': 74.6, 'Zoe': 70.8, 'Ben': 78.3
+    })
+
+    student_accuracy_week("Parsa Safaie",
+        {
+        'Monday': 85.5, 'Tuesday': 87.2, 'Wednesday': 88.1, 'Thursday': 90.4,
+        'Friday': 92.3, 'Saturday': 91.5, 'Sunday': 89.0
     })
