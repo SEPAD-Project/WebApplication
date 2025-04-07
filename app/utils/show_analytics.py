f96e71824b2b6ae918bcc2676e8f4c82bd4e0989
parsasafaie <parsasafaie.2568@proton.me>
2025-04-07 18:59:28 +0330

    add compare classes in analytics

---
+++ app/utils/show_analytics.py
@@ -0,0 +1,59 @@
+import plotly.express as px
+
+def bar_style(fig ,title, x_title, y_title):
+    fig.update_traces(
+        texttemplate='%{text:.0f}',
+        textposition='outside',
+        marker_color='#4ECDC4',
+    )
+
+    fig.update_layout(
+        bargap=0.5,
+
+        title=dict(
+            text=title,
+            font=dict(size=27, color='white', family='sans-serif'),
+            xanchor='center',
+            x=0.5
+        ),
+
+        font=dict(size=14, color='white', family='sans-serif'),
+
+        plot_bgcolor='#1E1E1E',
+        paper_bgcolor='#1E1E1E',
+
+        xaxis=dict(
+            title=dict(text=x_title, font=dict(color='white')),
+            showgrid=False,
+            tickfont=dict(color='white'),
+            linecolor='grey',
+            ticks='outside',
+        ),
+
+        yaxis=dict(
+            title=dict(text=y_title, font=dict(color='white')),
+            gridcolor='#444',
+            zeroline=False,
+            tickfont=dict(color='white'),
+            linecolor='grey'
+        ),
+    )
+
+    return fig
+
+
+def compare_classes(classes: dict):
+    fig = px.bar(
+        x=list(classes.keys()),
+        y=list(classes.values()),
+        text=list(classes.values()),
+        title="Compare Classes"
+    )
+
+    fig = bar_style(fig, title="Compare Classes", x_title="Class Code", y_title="Score")
+
+    fig.write_html("compare_classes.html")
+
+
+if __name__=="__main__":
+    compare_classes({'1051': 25.5, '1052': 84.3, '1053': 56.7, '1054': 96.3, '1051': 25.5, '1052': 84.3, '1053': 56.7, '1054': 96.3})
