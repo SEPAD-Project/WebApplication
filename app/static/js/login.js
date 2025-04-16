a17949f579fa33a4f39b14c3f8da2cec49fca0b4
parsasafaie <parsasafaie.2568@proton.me>
2025-04-16 16:50:29 +0330

    change login style

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,22 +1,13 @@
-const username_input = document.getElementById('username');
-const password_input = document.getElementById('password');
-
-username_input.addEventListener('focus', () => {
-username_input.placeholder = '';
-});
-
-username_input.addEventListener('blur', () => {
-if (!username_input.value) {
-    username_input.placeholder = 'username';
-}
-});
-
-password_input.addEventListener('focus', () => {
-password_input.placeholder = '';
-});
-
-password_input.addEventListener('blur', () => {
-if (!password_input.value) {
-    password_input.placeholder = 'password';
-}
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 50, density: { enable: true, value_area: 800 } },
+        color: { value: "#FFFFFF" },
+        shape: { type: "circle" },
+        opacity: { value: 0.5, random: true },
+        size: { value: 3, random: true },
+        move: { enable: true, speed: 3, direction: "none", random: true },
+    },
+    interactivity: {
+        events: { onhover: { enable: true, mode: "repulse" } },
+    },
 });
