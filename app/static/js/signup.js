cca3628ed2e24eff32662645066ff8d0c8195e0c
parsasafaie <parsasafaie.2568@proton.me>
2025-04-16 16:55:45 +0330

    change signup style

--- app/static/js/signup.js
+++ app/static/js/signup.js
@@ -1,63 +1,13 @@
-const school_name = document.getElementById('school-name');
-const school_code = document.getElementById('school-code');
-const manager_personal_code = document.getElementById('manager-personal-code');
-const province = document.getElementById('province');
-const city = document.getElementById('city');
-
-school_name.addEventListener('focus', () => {
-school_name.placeholder = '';
-});
-
-school_name.addEventListener('blur', () => {
-if (!school_name.value) {
-    school_name.placeholder = 'School name';
-}
-});
-
-//=============================================
-
-school_code.addEventListener('focus', () => {
-school_code.placeholder = '';
-});
-
-school_code.addEventListener('blur', () => {
-if (!school_code.value) {
-    school_code.placeholder = 'School code';
-}
-});
-
-//=============================================
-
-manager_personal_code.addEventListener('focus', () => {
-    manager_personal_code.placeholder = '';
-});
-    
-manager_personal_code.addEventListener('blur', () => {
-if (!manager_personal_code.value) {
-    manager_personal_code.placeholder = 'Manager personal code';
-}
-});
-
-//=============================================
-
-province.addEventListener('focus', () => {
-    province.placeholder = '';
-});
-    
-province.addEventListener('blur', () => {
-if (!province.value) {
-    province.placeholder = 'Province';
-}
-});
-
-//=============================================
-
-city.addEventListener('focus', () => {
-    city.placeholder = '';
-});
-    
-city.addEventListener('blur', () => {
-if (!city.value) {
-    city.placeholder = 'City';
-}
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 50, density: { enable: true, value_area: 800 } },
+        color: { value: "#FFFFFF" },
+        shape: { type: "circle" },
+        opacity: { value: 0.5, random: true },
+        size: { value: 3, random: true },
+        move: { enable: true, speed: 3, direction: "none", random: true },
+    },
+    interactivity: {
+        events: { onhover: { enable: true, mode: "repulse" } },
+    },
 });
