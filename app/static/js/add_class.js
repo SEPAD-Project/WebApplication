82c7d07572208e54fbca9094c1ecc23e35be3387
parsasafaie <parsasafaie.2568@proton.me>
2025-04-16 20:35:14 +0330

    change class routes style

--- app/static/js/add_class.js
+++ app/static/js/add_class.js
@@ -1,11 +1,23 @@
-const name_input = document.getElementById('class_name');
-
-name_input.addEventListener('focus', () => {
-name_input.placeholder = '';
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 50, density: { enable: true, value_area: 800 } },
+        color: { value: "#FFFFFF" },
+        shape: { type: "circle" },
+        opacity: { value: 0.5, random: true },
+        size: { value: 3, random: true },
+        move: { enable: true, speed: 3, direction: "none", random: true },
+    },
+    interactivity: {
+        events: { onhover: { enable: true, mode: "repulse" } },
+    },
 });
 
-name_input.addEventListener('blur', () => {
-if (!name_input.value) {
-    name_input.placeholder = 'Class Name';
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const fileInput = document.querySelector('.file-input-hidden');
+    const fileInputLabel = document.querySelector('.file-input-label');
+
+    fileInput.addEventListener('change', (event) => {
+        const fileName = event.target.files[0]?.name || 'Choose Excel File';
+        fileInputLabel.textContent = fileName;
+    });
 });
