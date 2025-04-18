9178f9eea77c9463b7bdd2e26f545e2e08092f6c
parsasafaie <parsasafaie.2568@proton.me>
2025-04-18 17:09:59 +0330

    update student routes

--- app/routes/student_routes.py
+++ app/routes/student_routes.py
@@ -9,7 +9,7 @@ from flask_login import current_user, login_required
 
 # Internal module imports
 from app import db  # SQLAlchemy instance for database operations
-from app.models.models import Student, Class
+from app.models.models import Student, Class, School
 from app.server_side.Website.directory_manager import (
     dm_create_student,
     dm_edit_student,
@@ -32,17 +32,17 @@ def panel_students():
     - If a search query (q) is provided, filters students by name, family name, or national code.
     - Otherwise, displays all students from the school.
     """
+    school = School.query.filter(School.school_code==current_user.school_code).first()
     query = request.args.get('q')
     if query:
         students = Student.query.filter(
             ((Student.student_name.ilike(f"%{query}%")) |
              (Student.student_family.ilike(f"%{query}%")) |
              (Student.student_national_code.ilike(f"%{query}%"))) &
-            (Student.school_code == current_user.school_code)
+            (Student.school_id == current_user.id)
         ).all()
     else:
-        students = Student.query.filter_by(
-            school_code=current_user.school_code).all()
+        students = school.students
 
     return render_template('student/students.html', students=students)
 
@@ -62,11 +62,14 @@ def add_student():
         student_national_code = request.form['student_national_code']
         student_password = request.form['student_password']
         class_code = request.form['selected_class']
-        school_code = current_user.school_code
+        school_id = current_user.id
         student_image = request.files['file_input']
+        
+        school_code = current_user.school_code
+        class_id = Class.query.filter(Class.class_code==class_code).first().id
 
         # Save the student image
-        image_path = f"c:/sap-project/server/schools/{school_code}/{reverse_class_code(class_code)[1]}/{student_national_code}.jpg"
+        image_path = f"c:/sap-project/server/schools/{current_user.school_code}/{reverse_class_code(class_code)[1]}/{student_national_code}.jpg"
         student_image.save(image_path)
 
         # Create and save the new student
@@ -75,8 +78,8 @@ def add_student():
             student_family=student_family,
             student_national_code=student_national_code,
             student_password=student_password,
-            class_code=class_code,
-            school_code=school_code
+            class_id=class_id,
+            school_id=school_id
         )
         try:
             db.session.add(new_student)
@@ -93,8 +96,8 @@ def add_student():
             return redirect(url_for('student_routes.duplicated_student_info'))
 
     # Render the form with available classes
-    classes = Class.query.filter_by(school_code=current_user.school_code).all()
-    return render_template('student/add_student.html', classes=classes)
+    school = School.query.filter(School.school_code==current_user.school_code).first()
+    return render_template('student/add_student.html', classes=school.classes)
 
 
 @bp.route('/panel/students/add_from_excel', methods=['GET', 'POST'])
@@ -108,12 +111,12 @@ def add_from_excel():
     if request.method == 'POST':
         global texts
 
+        school = School.query.filter(School.school_code==current_user.school_code).first()
+
         # Prepare references for validation
-        classes = Class.query.filter_by(
-            school_code=current_user.school_code).all()
+        classes = school.classes
         class_names = [c.class_name for c in classes]
-        students = Student.query.filter_by(
-            school_code=current_user.school_code).all()
+        students = school.students
         existing_ncs = [s.student_national_code for s in students]
 
         # Read file and mapping from user input
@@ -181,9 +184,9 @@ def add_from_excel():
                 student_name=student['name'],
                 student_family=student['family'],
                 student_national_code=student['national_code'],
-                class_code=student['class'],
+                class_id=Class.query.filter(Class.class_code==student['class']).first().id,
                 student_password=student['password'],
-                school_code=current_user.school_code
+                school_code=current_user.id
             )
             db.session.add(new_student)
 
@@ -220,12 +223,9 @@ def edit_student(student_national_code):
     - GET: Displays the form with current student data.
     - POST: Applies updates to the database and directory.
     """
-    student = Student.query.filter_by(
-        student_national_code=student_national_code,
-        school_code=current_user.school_code
-    ).first()
-
-    if not student:
+    school = School.query.filter(School.school_code==current_user.school_code).first()
+    student = Student.query.filter(Student.school_id==school.id).first()
+    if not student in school.students:
         session["show_error_notif"] = True
         return redirect(url_for("student_routes.unknown_student_info"))
 
@@ -249,8 +249,7 @@ def edit_student(student_national_code):
             session["show_error_notif"] = True
             return redirect(url_for('student_routes.duplicated_student_info'))
 
-    classes = Class.query.filter_by(school_code=current_user.school_code).all()
-    return render_template('student/edit_student.html', student=student, classes=classes)
+    return render_template('student/edit_student.html', student=student, classes=school.classes)
 
 
 @bp.route('/panel/students/remove_student/<student_national_code>', methods=['POST', 'GET'])
@@ -261,7 +260,7 @@ def remove_student(student_national_code):
     """
     student = Student.query.filter_by(
         student_national_code=student_national_code,
-        school_code=current_user.school_code
+        school_id=current_user.id
     ).first()
 
     if not student:
@@ -272,7 +271,7 @@ def remove_student(student_national_code):
     db.session.commit()
     dm_delete_student(
         school_code=current_user.school_code,
-        class_name=reverse_class_code(student.class_code)[1],
+        class_name=reverse_class_code(Class.query.filter(Class.id==student.class_code).first().class_code)[1],
         student_code=student_national_code
     )
     return redirect(url_for('student_routes.panel_students'))
@@ -286,7 +285,7 @@ def student_info(student_national_code):
     """
     student = Student.query.filter_by(
         student_national_code=student_national_code,
-        school_code=current_user.school_code
+        school_id=current_user.id
     ).first()
 
     if not student:
