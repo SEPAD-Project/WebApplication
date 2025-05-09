34bb2761c563f2580a6dd4ea9a4fd6e2cf8f4bfb
parsasafaie <parsasafaie.2568@proton.me>
2025-05-09 16:46:33 +0330

    auto update and git pull when use run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
+echo Pull codes from github and get the latest version
+git pull
 echo Starting Flask app with Waitress...
 echo Serving on http://0.0.0.0:2568
 waitress-serve --listen=0.0.0.0:2568 source.wsgi:app
