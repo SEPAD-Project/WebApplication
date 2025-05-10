a007a87ebbe717e05ef8623805e2edd0fc5da07e
parsasafaie <parsasafaie.2568@proton.me>
2025-05-10 07:22:06 +0330

    remove fetch from github in run.bat to see local changes too

--- run.bat
+++ run.bat
@@ -1,8 +1,4 @@
 @echo off
-echo fetch codes from github and get the latest version
-git fetch --all
-git reset --hard origin/main
-git clean -fd
 echo Starting Flask app with Waitress...
 echo Serving on http://0.0.0.0:2568
 waitress-serve --listen=0.0.0.0:2568 source.wsgi:app
