4685ac017f6e7a7c0d0f77363a9222689d75aabf
parsasafaie <parsasafaie.2568@proton.me>
2025-05-25 19:26:22 +0330

    update run tools for autorun celery at startup

--- run.bat
+++ run.bat
@@ -5,6 +5,8 @@ echo Fetch changes from github and clean repository
 git fetch --all
 git reset --hard origin/main
 git clean -fd 
+echo Starting Celery service...
+celery -A source.celery worker --loglevel=info -P threads
 echo Starting Flask app with Waitress...
 echo Serving on http://0.0.0.0:2568
 waitress-serve --listen=0.0.0.0:2568 source.app:app
--- run.py
+++ run.py
@@ -1,7 +1,18 @@
 #Use this for development server. for production server use run.bat
-print('Use this for development server. for production server use run.bat')
-
 from source.app import app
+import subprocess
 
 if __name__=='__main__':
+    print('Use this for development server. for production server use run.bat')
+
+    print('Starting Celery service..')
+    subprocess.Popen([
+        "celery", 
+        "-A", "source.celery", 
+        "worker", 
+        "--loglevel=info", 
+        "-P", "threads"
+    ])
+    
+    print('Starting Flask app with flask run tool...')
     app.run(host='0.0.0.0', port='2568', debug=True)
