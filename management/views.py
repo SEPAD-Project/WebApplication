78457b0636352023eb3f64880b9c0c85a1fb0909
parsasafaie <parsasafaie.2568@proton.me>
2025-07-20 07:39:59 +0330

    fix views bugs

--- management/views.py
+++ management/views.py
@@ -15,10 +15,10 @@ def contact(request):
     return render(request, 'contact.html')
 
 def comings_soon(request):
-    return render(request, 'comings_soon.html')
+    return render(request, 'coming_soon.html')
 
 def page_404(request):
-    return render(request, 'page_404.html')
+    return render(request, '404.html')
 
 def page_429(request):
-    return render(request, 'page_429.html')
+    return render(request, '429.html')
