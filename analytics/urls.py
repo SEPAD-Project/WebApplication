afd5a9fa198969d25dc2050294b044c87e79d52e
parsasafaie <parsasafaie.2568@proton.me>
2025-07-27 19:27:38 +0330

    add analytics generation process view and urls

--- analytics/urls.py
+++ analytics/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('/select_student_for_lesson_accuracy', views.select_student_for_lesson_accuracy, name='select_student_for_lesson_accuracy'),
     path('/select_student_for_week_accuracy', views.select_student_for_week_accuracy, name='select_student_for_week_accuracy'),
     path('/select_class_for_students_accuracy', views.select_class_for_students_accuracy, name='select_class_for_students_accuracy'),
+    path('/school_teachers_performance', views.school_teachers_performance, name='school_teachers_performance'),
+    path('/school_classes_accuracy', views.school_classes_accuracy, name='school_classes_accuracy'),
 ]
