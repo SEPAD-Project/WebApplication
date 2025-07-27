2111ec042dc23d36d586ae0ea5681b9a5642d6c3
parsasafaie <parsasafaie.2568@proton.me>
2025-07-27 17:07:16 +0330

    add analytics forms and views

--- analytics/urls.py
+++ analytics/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.analytics_menu, name='analytics_menu'),
+    path('/compare_student_accuracy_lesson', views.compare_student_accuracy_lesson, name='compare_student_accuracy_lesson'),
+    path('/compare_student_accuracy_week', views.compare_student_accuracy_week, name='compare_student_accuracy_week'),
+    path('/compare_students', views.compare_students, name='compare_students'),
 ]
