6ab96f6fea83955c3ace813bd794cfcb2784b133
parsasafaie <parsasafaie.2568@proton.me>
2025-07-28 19:39:44 +0330

    use delay method in celery

--- analytics/views.py
+++ analytics/views.py
@@ -25,7 +25,7 @@ def class_accuracy_report_view(request):
     if request.method == "POST":
         class_id = request.POST.get('selected_class')
 
-        task_generate_class_students_accuracy(
+        task_generate_class_students_accuracy.delay(
             school_id=current_user.id,
             class_id=class_id,
             school_email=current_user.email
@@ -47,7 +47,7 @@ def student_lesson_accuracy_report_view(request):
 
         student = Student.objects.filter(student_national_code=student_nc).first()
         if student:
-            task_generate_student_accuracy_by_lesson(
+            task_generate_student_accuracy_by_lesson.delay(
                 school_id=current_user.id,
                 name=student.student_name,
                 family=student.student_family,
@@ -68,7 +68,7 @@ def student_week_accuracy_report_view(request):
 
         student = Student.objects.filter(student_national_code=student_nc).first()
         if student:
-            task_generate_student_accuracy_by_week(
+            task_generate_student_accuracy_by_week.delay(
                 school_id=current_user.id,
                 name=student.student_name,
                 family=student.student_family,
@@ -85,7 +85,7 @@ def student_week_accuracy_report_view(request):
 def school_teachers_performance_report_view(request):
     current_user = request.user
 
-    task_generate_school_teachers_performance(
+    task_generate_school_teachers_performance.delay(
         school_id=current_user.id,
         school_email=current_user.email
     )
@@ -98,7 +98,7 @@ def school_teachers_performance_report_view(request):
 def school_classes_accuracy_report_view(request):
     current_user = request.user
 
-    task_generate_school_classes_accuracy(
+    task_generate_school_classes_accuracy.delay(
         school_id=current_user.id,
         school_email=current_user.email
     )
