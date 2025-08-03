e9e7d503ecdbf883e5c02ab0c3e81b1cbd35b620
parsasafaie <parsasafaie.2568@proton.me>
2025-08-03 21:15:49 +0330

    update stop_server.bat for close old process windows

--- scripts/stop_server.bat
+++ scripts/stop_server.bat
@@ -1,6 +1,16 @@
 @echo off
+setlocal enabledelayedexpansion
+
 echo Stopping Django runserver...
-for /f "tokens=2" %%a in ('tasklist ^| findstr "manage.py"') do taskkill /PID %%a /F
+for /f "tokens=2,3 delims=," %%a in ('wmic process where "CommandLine like '%%manage.py%%'" get ProcessId^,ParentProcessId /format:csv ^| findstr /i "manage.py"') do (
+    echo Terminating PID %%a and parent CMD %%b
+    taskkill /PID %%a /F >nul
+    taskkill /PID %%b /F >nul
+)
 
 echo Stopping Celery worker...
-for /f "tokens=2" %%a in ('tasklist ^| findstr "celery"') do taskkill /PID %%a /F
+for /f "tokens=2,3 delims=," %%a in ('wmic process where "CommandLine like '%%celery%%'" get ProcessId^,ParentProcessId /format:csv ^| findstr /i "celery"') do (
+    echo Terminating PID %%a and parent CMD %%b
+    taskkill /PID %%a /F >nul
+    taskkill /PID %%b /F >nul
+)
